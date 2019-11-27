90f402c6142c6e18b85c9edb33693f9d62b5a126
Samuel Jimenez <sjimenezre@gmail.com>
2019-11-27 23:21:32 +0100

    Main and test

--- main.go
+++ main.go
@@ -4,6 +4,27 @@ import (
 	"fmt"
 )
 
+type Person struct {
+	Name string
+	Age  int
+}
+
+func checkAge(age int) bool {
+	return age >= 18
+}
+
+func sellBeer(person Person) {
+	if checkAge(person.Age) {
+		fmt.Println(person.Name, "can buy beer")
+	} else {
+		fmt.Println(person.Name, "CAN'T buy beer")
+	}
+}
+
 func main() {
-	fmt.Println("Hey!")
+	person := Person{
+		Name: "Samuel",
+		Age:  19,
+	}
+	sellBeer(person)
 }
---
+++ main_test.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestCheckAge(t *testing.T) {
+	personOverAge := Person{
+		Age: 19,
+	}
+	personUnderAge := Person{
+		Age: 15,
+	}
+	if !checkAge(personOverAge.Age) {
+		t.Errorf("Error in checkAge")
+	}
+	if checkAge(personUnderAge.Age) {
+		t.Errorf("Error in checkAge")
+	}
+
+}
